2643d8a6409dd9363c97ce4c1984c0fb8d5a70b9
cryskram <vageeshgn2005@gmail.com>
2025-07-04 00:13:12 +0530

    added loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
-import Image from "next/image";
+"use client";
+
+import Hero from "@/components/Hero";
+import Loader from "@/components/Loader";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true);
+      setTimeout(() => setLoading(false), 500);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        className={`flex flex-col w-full min-h-screen items-center justify-center transition-opacity duration-500 ${
+          fadeOut ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-gradient-to-br from-gray-700 to-gray-900 w-full min-h-screen flex flex-col items-center justify-center">
-      <h1 className="font-bold text-4xl md:text-7xl animate-pulse">
-        Coming<span className="text-amber-500">Soon</span>
-      </h1>
+    <div className="flex flex-col items-center justify-center px-4 w-full">
+      <Hero />
     </div>
   );
 }
